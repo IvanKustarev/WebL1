4a5d1e837900d029eb08f0a47489c37ff640f0d9
IvanKustarev <ivan.kustarev@gmail.com>
2021-08-24 22:58:26 +0300

    Working php-script

--- check.php
+++ check.php
@@ -1,5 +1,6 @@
 <?php
     if(isset($_POST['R']) && isset($_POST['X']) && isset($_POST['Y'])){
+        $start_time = time();
         $R = $_POST['R'];
         $X = $_POST['X'];
         $Y = $_POST['Y'];
@@ -7,15 +8,35 @@
 
         if($X < 0 && $Y > 0){
             $into = false;
-            echo "$into";
+        }
+        if($X >= 0 && $Y>=0){
+            if(pow(pow($X,2)+pow($Y,2), 1/2) <= $R){
+                $into = true;
+            }else{
+                $into = false;
+            }
+        }
+        if($X >=0 && $Y <= 0){
+//            y = R/4*x - R/2 - уравнение прямой в четвёртой полуплоскости (R/4*x - y - R/2 = 0)
+            if($R/4*$X - $Y - $R/2 <= 0){
+                $into = true;
+            }else{
+                $into = false;
+            }
+        }
+        if($X <= 0 && $Y <= 0){
+            if($X >= -$R/2 && $Y >= -$R){
+                $into = true;
+            }else{
+                $into = false;
+            }
         }
 
-//        <form action="check.php" method="post">
-//        <input type="text" name="R" tabindex="1" placeholder="Параметр R">
-//        <input type="text" name="X" tabindex="2" placeholder="Параметр X">
-//        <input type="text" name="Y" tabindex="3" placeholder="Параметр Y">
-//        <button type="submit">Проверить точку</button>
-//    </form>
+        if($into){
+            echo "true";
+        }else{
+            echo "false";
+        }
     }
 ?>
 <!doctype html>
